54ec2fbfd25aa7223fe2d0d98b47147c2102349f
Biiel22 <ga.ga.visentin@gmail.com>
2023-03-31 17:28:09 -0300

    Consulta de dados corretos

--- Principal/consulta.php
+++ Principal/consulta.php
@@ -1,17 +1,23 @@
 <?php
 
-    require_once 'conexao.php';
+    require_once '../conexao/conexao.php';
 
 
-    $stmt = $conexao->prepare("SELECT * FROM EMD101");
+    $stmt = $conexao->prepare("SELECT DISTINCT EMD101.NOME, CRD111.DOCUMENTO, CRD111.VALOR AS VALOR_A_PAGAR, BXD111.VALOR AS VALOR_PAGO, CRD111.EMISSAO AS LANCTO, CRD111.VENCTO, BXD111.BAIXA AS DT_CONF_PAG
+        FROM EMD101
+        JOIN CRD111 ON EMD101.CGC_CPF=CRD111.CGC_CPF
+        LEFT JOIN BXD111 ON CRD111.DOCUMENTO=BXD111.DOCUMENTO
+        ORDER BY EMD101.NOME ASC;");
     $stmt->execute();
     $resultado = $stmt->fetchAll();
 
-    $codigo = ($resultado[0]["NOME"]);
-    $CGC = ($resultado[0]["CGC_CPF"]);
-    $DTCAD = ($resultado[0]["DT_CADASTRO"]);
-    print_r($codigo . "<br>");
-    print_r($CGC . "<br>");
-    print_r($DTCAD . "<br>");
-    
+    for($i = 0; $i < count($resultado); $i++) {
+        // print_r($resultado[$i]["NOME"]);
+        $nome = $resultado[$i]["NOME"];
+        $documento = $resultado[$i]["DOCUMENTO"];
+        $valor = $resultado[$i]["VALOR_A_PAGAR"];
+        print_r($nome."->");
+        print_r($documento."->");
+        print_r($valor."-> <br>");
+    }
 ?>
