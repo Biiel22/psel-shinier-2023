d2484ef7fc85746259c42001d69ffad70f2c1ae1
Biiel22 <ga.ga.visentin@gmail.com>
2023-03-31 18:54:22 -0300

    Criando json codigos das colunas excel

--- Principal/consulta.php
+++ Principal/consulta.php
@@ -5,6 +5,17 @@
 
     use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
+    $codigos_excel = array(
+        "nome" => "B",
+        "documento" => "C",
+        "dinheiro" => "D",
+        "valor_a_pagar" => "E",
+        "valor_pago" => "F",
+        "dt_lancto" => "G",
+        "vencto" => "H",
+        "dt_conf_pag" => "I"
+    );  
+
     $stmt = $conexao->prepare("SELECT DISTINCT EMD101.NOME, CRD111.DOCUMENTO, CRD111.VALOR AS VALOR_A_PAGAR, BXD111.VALOR AS VALOR_PAGO, CRD111.EMISSAO AS LANCTO, CRD111.VENCTO, BXD111.BAIXA AS DT_CONF_PAG
         FROM EMD101
         JOIN CRD111 ON EMD101.CGC_CPF=CRD111.CGC_CPF
@@ -15,6 +26,8 @@
 
     $excel = \PhpOffice\PhpSpreadsheet\IOFactory::load("financeiro.xlsx");
     $planilha = $excel->getActiveSheet();
+
+    $linha_excel = 3;
 
     for($i = 0; $i < count($resultado); $i++) {
         // print_r($resultado[$i]["NOME"]);
@@ -28,7 +41,10 @@
 
         $planilha->setCellValue('B3', $resultado[2]['NOME']);
     }
+
     $writer = new Xlsx($excel);
     $writer->save('financeiro.xlsx');
+
+    
 
 ?>
