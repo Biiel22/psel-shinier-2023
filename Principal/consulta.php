8eba085da1171a1dff17b9be3c9766b7295dc772
Biiel22 <ga.ga.visentin@gmail.com>
2023-03-31 18:34:16 -0300

    Testando biblioteca: PHPOffice

--- Principal/consulta.php
+++ Principal/consulta.php
@@ -1,7 +1,9 @@
 <?php
 
     require_once '../conexao/conexao.php';
+    require_once '../vendor/autoload.php';
 
+    use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
     $stmt = $conexao->prepare("SELECT DISTINCT EMD101.NOME, CRD111.DOCUMENTO, CRD111.VALOR AS VALOR_A_PAGAR, BXD111.VALOR AS VALOR_PAGO, CRD111.EMISSAO AS LANCTO, CRD111.VENCTO, BXD111.BAIXA AS DT_CONF_PAG
         FROM EMD101
@@ -11,13 +13,22 @@
     $stmt->execute();
     $resultado = $stmt->fetchAll();
 
+    $excel = \PhpOffice\PhpSpreadsheet\IOFactory::load("financeiro.xlsx");
+    $planilha = $excel->getActiveSheet();
+
     for($i = 0; $i < count($resultado); $i++) {
         // print_r($resultado[$i]["NOME"]);
         $nome = $resultado[$i]["NOME"];
         $documento = $resultado[$i]["DOCUMENTO"];
-        $valor = $resultado[$i]["VALOR_A_PAGAR"];
-        print_r($nome."->");
-        print_r($documento."->");
-        print_r($valor."-> <br>");
+        $valor_a_pagar = $resultado[$i]["VALOR_A_PAGAR"];
+        $valor_pago = $resultado[$i]["VALOR_PAGO"];
+        $dt_lancto = $resultado[$i]["LANCTO"];
+        $vencimento = $resultado[$i]["VENCTO"];
+        $dt_conf_pag = $resultado[$i]["DT_CONF_PAG"];
+
+        $planilha->setCellValue('B3', $resultado[2]['NOME']);
     }
+    $writer = new Xlsx($excel);
+    $writer->save('financeiro.xlsx');
+
 ?>
